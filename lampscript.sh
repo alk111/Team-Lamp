f2a1ebebd5b02344596e1fefc8748b4b51a328ab
alk111 <khanalkaif2001@gmail.com>
2023-01-12 10:53:00 +0530

    php setup is ready to go

--- lampscript.sh
+++ lampscript.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 <<<<<<< HEAD
-phpinstall=$(sudo apt install openssl php-common php-curl php-json php-mbstring php-mysql php-xml php-zip)
+phpinstall=$(sudo apt -y install php8.1)
+phpcheckversion=$(php -v)
+phpmodules=$(sudo apt-get install -y php8.1-cli php8.1-json php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath)
 
-echo  Installing PHP & Requirements ..$phpinstall
+echo  Installing PHP ..$phpinstall
+
+echo Checking PHP installed version..$phpcheckversion
+
+echo Installing Additional Modules ..$phpmodules
+
+
+
 =======
 echo  Installing phpMyAdmin...
 sudo apt-get install phpmyadmin -y
