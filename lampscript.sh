3663def8614cbbd70a406edf5784aabd31d2a70a
alk111 <khanalkaif2001@gmail.com>
2023-01-12 09:46:17 +0530

    php & req.. script added

--- lampscript.sh
+++ lampscript.sh
@@ -1,1 +1,5 @@
-Hello fork this file
+#!/bin/bash
+
+phpinstall=$(sudo apt install openssl php-common php-curl php-json php-mbstring php-mysql php-xml php-zip)
+
+echo  Installing PHP & Requirements ..$phpinstall
